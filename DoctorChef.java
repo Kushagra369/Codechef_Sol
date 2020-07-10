70b7183b85a5043e9db1ae6ce7eeed9e6b4b3c7a
Kushagra Saxena <64200696+Kushagra369@users.noreply.github.com>
2020-07-10 19:51:11 +0530

    Update DoctorChef.java

--- DoctorChef.java
+++ DoctorChef.java
@@ -1,9 +1,70 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class DoctorChef {
+class DoctorChef {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static class FastReader 
+	{
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() 
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() 
+		{
+			while (st == null || !st.hasMoreElements()) 
+			{
+				try 
+				{
+					st = new StringTokenizer(br.readLine());
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() 
+		{
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() 
+		{
+			return Long.parseLong(next());
+		}
+
+		double nextDouble()
+		{
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() 
+		{
+			String str = "";
+			try 
+			{
+				str = br.readLine();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		FastReader sc = new FastReader();
 		int t = sc.nextInt();
 		while(t-->0)
 		{
@@ -18,44 +79,36 @@ public class DoctorChef {
 				more.add(z);
 			}
 			
-			Collections.sort(more);
-			
 			if(more.get(0)<=x)
 			{
 				days = more.size();
 			}
 			else
 			{
-				for(int i: more)
+				int i = more.get(0);
+				int initpop = i;
+				while(i>0)
 				{
-					int initpop = i;
-					while(i>0)
+					if(x>i)
 					{
-						if(x>i)
+						x = 2*x;
+						i = 0;
+						days++;
+					}
+					else
+					{
+						i = i-x;
+						x = 2*x;
+						days++;
+						if(i!=0)
 						{
-							x = i;
-							i = 0;
-							days++;
-							x = 2*x;
-						}
-						else
-						{
-							i = i-x;
-							x = 2*x;
-							days++;
-							if(i!=0)
-							{
-								i = Math.min(initpop, 2*i);
-							}
+							i = Math.min(initpop, 2*i);
 						}
 					}
 				}
+				days = days + (n-1);
 			}
-			
 			System.out.println(days);
-			
 		}
-		sc.close();
 	}
-
 }
